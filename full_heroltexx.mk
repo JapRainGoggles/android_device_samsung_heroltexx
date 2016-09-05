8d3ebfabdd85316af95d225f221163d2f34946fb
King_lilrowrow <aarondoe02@icloud.com>
2016-09-05 20:34:34 +0100

    Initial flat S7 device source

--- full_heroltexx.mk
+++ full_heroltexx.mk
@@ -18,12 +18,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
-# Inherit from hero2ltexx device
-$(call inherit-product, device/samsung/hero2ltexx/device.mk)
+# Inherit from heroltexx device
+$(call inherit-product, device/samsung/heroltexx/device.mk)
 
 # Set those variables here to overwrite the inherited values.
-PRODUCT_NAME := full_hero2ltexx
-PRODUCT_DEVICE := hero2ltexx
+PRODUCT_NAME := full_heroltexx
+PRODUCT_DEVICE := heroltexx
 PRODUCT_BRAND := samsung
 PRODUCT_MANUFACTURER := samsung
-PRODUCT_MODEL := hero2ltexx
+PRODUCT_MODEL := heroltexx
